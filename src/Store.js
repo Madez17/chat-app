1279602492a6d7e756c71fe83a45564d224a908e
Madez <781@holbertonschool.com>
2019-11-27 05:01:02 +0000

    [Update chat] - random user

--- src/Store.js
+++ src/Store.js
@@ -39,13 +39,17 @@ function sendChatAction(value) {
 }
 
 export default function Store(props) {
+    
+    const [allChats, dispatch] = React.useReducer(reducer, initState);
+        
     if (!socket) {
-        socket = io(':3001')
+        socket = io(':3001');
+        socket.on('chat message', function(msg){
+            dispatch({type: 'RECEIVE_MESSAGE', payload: msg});
+        });
     }
 
     const user = 'Mafe' + Math.random(100).toFixed(2)
-
-    const [allChats] = React.useReducer(reducer, initState);
 
     return (
         <CTX.Provider value={{allChats, sendChatAction, user}}>
